f5c1b2d66fcbbe5acf43292824bd11e2c95b4475
Ben Trevor <benjamin.trevor@gmail.com>
2013-07-27 13:56:58 -0500

    Spike archives route

--- sinatra_app.rb
+++ sinatra_app.rb
@@ -2,6 +2,23 @@ require 'sinatra'
 
 class SinatraApp < Sinatra::Base
   get '/' do
-    erb :home
+    @latest = File.read(File.open("public/entries/test.html"))
+    erb :latest
+  end
+
+  get '/archives' do
+    @entries = all_entries
+    erb :archives
+  end
+
+  def all_entries
+    entries = []
+
+    Dir.foreach("public/entries") do |entry|
+      next if entry == '.' or entry == '..'
+      entries << entry
+    end
+
+    entries
   end
 end
